91f45c8b81c95403c3ccc0097281e6f5e0288d03
Pedro Northon Nobile <nobile.pedro@gmail.com>
2018-08-17 18:00:16 -0300

    Segundo commit - Alterações nos arquivos de código Java e layout.

--- app/src/main/java/br/edu/ifsp/scl/sdm/olamundo/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/olamundo/MainActivity.java
@@ -2,8 +2,10 @@ package br.edu.ifsp.scl.sdm.olamundo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView olaMundoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
